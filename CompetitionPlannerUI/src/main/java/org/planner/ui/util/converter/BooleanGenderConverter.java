de7df6a7e8f92f7a6b1a497d6a514ea41d707d9e
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-05 23:04:47 +0200

    initial

---
+++ CompetitionPlannerUI/src/main/java/org/planner/ui/util/converter/BooleanGenderConverter.java
@@ -0,0 +1,30 @@
+package org.planner.ui.util.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+
+import org.planner.model.Gender;
+
+@Named
+public class BooleanGenderConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		if ("false".equals(value))
+			return Gender.m;
+		if ("true".equals(value))
+			return Gender.w;
+		throw new IllegalArgumentException(value);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		if (value == Gender.m)
+			return "false";
+		if (value == Gender.w)
+			return "true";
+		return null;
+	}
+}
