6c954645e75419c651d96c85b3a4f3633ab497a8
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-06 19:29:34 +0200

    geht erstmal

--- CompetitionPlannerUI/src/main/webapp/resources/js/application.js
+++ CompetitionPlannerUI/src/main/webapp/resources/js/application.js
@@ -1,3 +1,163 @@
+function selectSidebarLink(link) {
+	$("#leftMenu").find(".ui-state-active").removeClass("ui-state-active");
+	if (link) {
+		$(link).addClass("ui-state-active");
+	}
+	hideHelp();
+}
+function navItemSelected() {
+	$("#leftMenuContainer").css("display", "").addClass("menuHidden");
+}
+function hideHelp() {
+	PF("layout").hide("east");
+}
+function toggleHelp() {
+	var layout = PF("layout");
+	if (layout.layout.east.state.isVisible)
+		layout.hide("east");
+	else
+		layout.show("east");
+}
+function sendLogin(msgError) {
+	var dlg = PF("loginDlg");
+	var req = createXMLHttpRequest();
+	req.open("POST", "j_security_check");
+	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+	req.onreadystatechange = function() {
+		if (req.readyState == 4) {
+			dlg.jq.find("input,button").attr("disabled", false).attr("readonly", false);
+			if (req.status == 200 && req.responseText.indexOf("<title>" + dlg.jq.find("#loginDialog_title").text() + "</title>") == -1) {
+				message("");
+				dlg.hide();
+				//	var ext;
+				//	PrimeFaces.ajax.AjaxRequest(dlg.cfg, ext);
+				refresh();							
+			} else {
+				message(msgError);
+			}
+		}
+	}
+	dlg.jq.find("input,button").attr("disabled", true).attr("readonly", true);
+	req.send("j_username=" + dlg.jq.find("input[type=text]").val() + String.fromCharCode(38) + "j_password=" + dlg.jq.find('input[type=password]').val());
+}
+function sendRecovery(userMsg, errorMsg) {
+	var dlg = PF("loginDlg");
+	var user = dlg.jq.find("input[type=text]").val();
+	if (user == "") {
+		message(userMsg);
+	} else {
+		var req = createXMLHttpRequest();
+		req.open("POST", "passwordreset?user=" + user);
+		req.onreadystatechange = function() {
+			if (req.readyState == 4) {
+				if (req.status == 200)
+					message(req.responseXML.firstChild.textContent);
+				else
+					message(errorMsg);
+			}
+		}
+		req.send();
+	}
+}
+function createXMLHttpRequest() { 
+	if (typeof XMLHttpRequest != "undefined")
+		return new XMLHttpRequest();
+	else if (typeof ActiveXObject != "undefined")
+		return new ActiveXObject("Microsoft.XMLHTTP");
+	else
+		throw new Error("XMLHttpRequest not supported");
+}
+function message(msg) {
+	$(".loginMessage").html(msg);
+}
+function emptyResponse() {
+	return "<?xml version='1.0' encoding='UTF-8'?><partial-response />";
+}
+function setUrlParam(val) {
+	val = val !== undefined ? "?i=" + encodeURIComponent(val) : location.protocol + "//" + location.host + location.pathname;
+	if (history.replaceState)
+		history.replaceState("", "", val);
+}
+function registrationEdit_enableButtons() {
+	var racesTable = PF("racesTable");
+	var athletesTable = PF("athletesTable");
+	var registrationTable = PF("registrationTable");
+	if ((athletesTable && athletesTable.getSelectedRowsCount() > 0) &&
+			((racesTable && racesTable.getSelectedRowsCount() > 0)
+				|| (registrationTable && registrationTable.getSelectedRowsCount() > 0)))
+		PF("btnAdd").enable();
+	else
+		PF("btnAdd").disable();
+}
+function initBirtDate() {
+	var date = new Date(new Date().getFullYear() - 10, 0, 1, 12);
+	PF("birtDate").setDate(date);
+}
+function initTableDND(sourceTable, targetTable) {
+	sourceTable.draggable({
+		helper: "clone",
+		scope: "treetotable",
+		zIndex: ++PrimeFaces.zindex
+	});
+	targetTable.droppable({
+		activeClass: "ui-state-active",
+		hoverClass: "ui-state-highlight",
+		tolerance: "pointer",
+		scope: "treetotable",
+		drop: function(event, ui) {
+			var property = ui.draggable.find('td').text(),
+				droppedColumnId = $(this).parents('th:first').attr('id'),
+				dropPos = $(this).hasClass('dropleft') ? 0 : 1;
+
+			treeToTable([
+				{name: 'property', value:  property}
+				,{name: 'droppedColumnId', value: droppedColumnId}
+				,{name: 'dropPos', value: dropPos}
+			]);
+		}
+	});
+
+	$('.ui-datatable th').draggable({
+		scope: 'tabletotree',
+		helper: function() {
+			var th = $(this);
+
+			return th.clone().appendTo(document.body).css('width', th.width());
+		}
+	});
+
+	$('.ui-tree').droppable({
+		helper: 'clone',
+		scope: 'tabletotree',
+		activeClass: 'ui-state-active',
+		hoverClass: 'ui-state-highlight',
+		tolerance: 'touch',
+		drop: function(event, ui) {                               
+			tableToTree([
+				{name: 'colIndex', value:  ui.draggable.index()}
+			]);
+		}
+	});
+}
+function setupAjax(loginTitle) {
+	$.ajaxSetup({
+		dataFilter: function(data) {
+			$(document).off("keydown");
+			if (data.indexOf(loginTitle) != -1) {
+				PrimeFaces.debug("Detected unauthenticated request");
+				var dlg = PF("loginDlg");
+				dlg.jq.find("input[type=text]").val("");
+				dlg.jq.find('input[type=password]').val("");
+				dlg.show();
+				data = emptyResponse();
+			}
+			return data;
+		}
+	});
+}
+$(window).resize(function() {
+	$("#leftMenuContainer").css("display", "");
+});
 PrimeFaces.locales["de"] = {
 	closeText: "Schließen",
 	prevText: "Zurück",
@@ -24,36 +184,3 @@ PrimeFaces.locales["de"] = {
 	day: "Tag",
 	allDayText: "Ganzer Tag"
 };
-function login1() {
-	$.ajax({
-		method: "POST",
-		url: "j_security_check",
-			data: "j_username=" + dlg.jq.find("input[type=text]").val() + "&j_password=" + dlg.jq.find('input[type=password]').val()
-	}).done(function(data) {
-		if (typeof data == "string" && data.indexOf(dlg.jq.find("#loginDialog_title").text()) != -1)
-			return;
-		dlg.hide();
-		var ext;
-		PrimeFaces.ajax.AjaxRequest(dlg.cfg, ext);
-//		$.ajax({
-//			method: dlg.cfg.type,
-//			url: dlg.cfg.url,
-//			data: dlg.cfg.data
-////		}).beforeSend(function(xhr, settings) {
-////			dlg.xhr.beforeSend(xhr, settings);
-//		}).error(function(xhr, textStatus, error) {
-//			dlg.xhr.error(xhr, textStatus, error);
-//		}).success(function(data, textStatus, xhr) {
-//			dlg.xhr.success(data, textStatus, xhr);
-//		}).complete(function(xhr, textStatus) {
-//			dlg.xhr.complete(xhr, textStatus);
-//		});
-	}).fail(function() {
-		dlg.jq.find(".loginError").show();
-	}).always(function() {
-		dlg.jq.find(".ui-button").attr("disabled", false);
-	});
-}
-function emptyResponse() {
-	return "<?xml version='1.0' encoding='UTF-8'?><partial-response />";
-}
