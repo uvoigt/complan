ff2ad02879aef900d2ed903bb42827a3be7f9ab2
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-06 19:07:53 +0200

    geht erstmal

--- CompetitionPlannerEJBClient/src/main/java/org/planner/eo/Address.java
+++ CompetitionPlannerEJBClient/src/main/java/org/planner/eo/Address.java
@@ -2,23 +2,22 @@ package org.planner.eo;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.xml.bind.annotation.XmlRootElement;
 
 import org.planner.util.Visible;
 
 @Entity
 @Access(AccessType.FIELD)
-@XmlRootElement
 public class Address extends AbstractEntity {
 	private static final long serialVersionUID = 1L;
 
-	@ManyToOne(optional = false)
+	@ManyToOne(optional = false, cascade = CascadeType.PERSIST)
 	private Country country;
 
-	@ManyToOne(optional = false)
+	@ManyToOne(optional = false, cascade = CascadeType.PERSIST)
 	@Visible
 	private City city;
 
