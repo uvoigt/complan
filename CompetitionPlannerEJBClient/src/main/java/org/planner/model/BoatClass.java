8abceb62436db8069cd0bb29d894fcf8ae9f8b33
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-10 20:10:39 +0200

    helper-Methoden

--- CompetitionPlannerEJBClient/src/main/java/org/planner/model/BoatClass.java
+++ CompetitionPlannerEJBClient/src/main/java/org/planner/model/BoatClass.java
@@ -9,4 +9,58 @@ public enum BoatClass implements LocalizedEnum {
 	public String getText() {
 		return CommonMessages.getEnumText(this);
 	}
+
+	public int getMinimalTeamSize() {
+		switch (this) {
+		default:
+			throw new IllegalArgumentException(name());
+		case c1:
+		case k1:
+			return 1;
+		case c2:
+		case k2:
+			return 2;
+		case c4:
+		case k4:
+			return 4;
+		case c8:
+			return 8;
+		}
+	}
+
+	public int getMaximalTeamSize() {
+		switch (this) {
+		default:
+			throw new IllegalArgumentException(name());
+		case c1:
+		case k1:
+			return 1;
+		case c2:
+		case k2:
+			return 2;
+		case c4:
+		case k4:
+			return 4;
+		case c8:
+			return 8;
+		}
+	}
+
+	public int getAllowedSubstitutes() {
+		switch (this) {
+		default:
+			throw new IllegalArgumentException(name());
+		case c1:
+		case k1:
+			return 0;
+		case c2:
+		case k2:
+			return 1;
+		case c4:
+		case k4:
+			return 2;
+		case c8:
+			return 3;
+		}
+	}
 }
