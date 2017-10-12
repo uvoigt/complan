498b655052abb05b8f77b3c217d1b08ada0797fb
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-12 18:28:15 +0200

    weiterer Fortschritt, aber erst 10% fertig

--- CompetitionPlannerEJB/src/main/java/org/planner/business/ProgramServiceImpl.java
+++ CompetitionPlannerEJB/src/main/java/org/planner/business/ProgramServiceImpl.java
@@ -2,15 +2,14 @@ package org.planner.business;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.planner.business.CommonImpl.Operation;
+import org.planner.dao.PlannerDao;
 import org.planner.eo.Announcement;
 import org.planner.eo.Club;
 import org.planner.eo.Participant;
@@ -32,6 +31,9 @@ public class ProgramServiceImpl {
 	@Inject
 	private CommonImpl common;
 
+	@Inject
+	private PlannerDao dao;
+
 	public Long createProgram(Program program) {
 		// gibt es bereits ein Programm für diese Ausschreibung? dann nimm dieses
 		Suchkriterien krit = new Suchkriterien();
@@ -42,56 +44,87 @@ public class ProgramServiceImpl {
 		if (programs.size() > 0)
 			return programs.get(0).getId();
 
-		Announcement announcement = common.getById(Announcement.class, program.getAnnouncement().getId(), 0);
-		common.checkWriteAccess(program, Operation.create); // TODO
-
-		// kopiere sämtliche Rennen der Ausschreibung
-		Set<ProgramRace> races = new HashSet<>();
-		for (Race race : announcement.getRaces()) {
-			ProgramRace copiedRace = new ProgramRace();
-			copiedRace.setRace(race);
-			races.add(copiedRace);
-		}
-		program.setRaces(races);
+		common.checkWriteAccess(program, Operation.create);
 
 		common.save(program);
 
 		return program.getId();
 	}
 
-	public Program generateProgram(Program program) {
+	public void generateProgram(Program program) {
 		common.checkWriteAccess(program, Operation.save);
 
 		// lade alle Meldungen
 		Suchkriterien crit = new Suchkriterien();
 		crit.addFilter(Registration_.announcement.getName(), program.getAnnouncement().getId());
 		crit.addFilter(Registration_.status.getName(), RegistrationStatus.submitted);
-		List<Registration> registrations = common.search(Registration.class, crit).getListe();
-		Map<Long, Set<Team>> races = new HashMap<>();
+		List<Registration> registrations = dao.search(Registration.class, crit, null).getListe();
+		Map<Long, List<Team>> races = new HashMap<>();
 
 		// hier werden erst einmal alle Meldungen der Map hinzugefügt
-		// am Ende ->
+		// eine Startbahn (lane) ist noch nicht zugeordnet!
 		for (Registration registration : registrations) {
 			for (RegEntry entry : registration.getEntries()) {
 				addEntryToProgram(registration.getClub(), entry, races);
 			}
 		}
+		Announcement announcement = common.getById(Announcement.class, program.getAnnouncement().getId(), 0);
+
+		int numberOfLanes = 8; // TODO announcement.get.. tracksSprint
+		program.setRaces(new ArrayList<ProgramRace>());
+
 		// simples Hinzufügen eines Laufs ... das wird erweitert!
-		for (ProgramRace programRace : program.getRaces()) {
-			Long raceId = programRace.getRace().getId();
+		for (Race race : announcement.getRaces()) {
+			Long raceId = race.getId();
 			// das sind alle Teams, die für das Rennen gemeldet haben
-			Set<Team> teams = races.get(raceId);
-			programRace.setParticipants(teams);
+			List<Team> teams = races.get(raceId);
+			// die Teams werden nun auf die verfügbaren Startbahnen verteilt
+
+			// upps.. hat niemand für dieses Rennen gemeldet?
+			if (teams == null)
+				continue;
+
+			List<ProgramRace> programRaces = calculateHeats(numberOfLanes, race, teams);
+			program.getRaces().addAll(programRaces);
 		}
 
-		return common.save(program);
+		common.save(program);
 	}
 
-	private void addEntryToProgram(Club club, RegEntry entry, Map<Long, Set<Team>> races) {
+	private List<ProgramRace> calculateHeats(int numberOfLanes, Race race, List<Team> teams) {
+		List<ProgramRace> programRaces = new ArrayList<>();
+
+		int numberOfHeats = teams.size() / numberOfLanes;
+		int remainder = teams.size() % numberOfLanes;
+
+		// TODO was soll passieren, wenn weniger als ein halbes Rennen übrigbleibt?
+		if (remainder < numberOfLanes / 2) {
+
+		}
+
+		for (int i = 0, teamOffset = 0; i < numberOfHeats; i++) {
+			ProgramRace programRace = new ProgramRace();
+			programRace.setRace(race);
+			programRace.setNumber(i + 1);
+			int endOffset = Math.min(teamOffset + numberOfLanes, teams.size() - 1);
+			List<Team> participants = teams.subList(teamOffset, endOffset);
+			int lane = 1;
+			for (Team team : participants) {
+				team.setLane(lane++);
+			}
+			programRace.setParticipants(participants);
+			programRaces.add(programRace);
+			teamOffset = endOffset;
+		}
+
+		return programRaces;
+	}
+
+	private void addEntryToProgram(Club club, RegEntry entry, Map<Long, List<Team>> races) {
 		Race race = entry.getRace();
-		Set<Team> teams = races.get(race.getId());
+		List<Team> teams = races.get(race.getId());
 		if (teams == null)
-			races.put(race.getId(), teams = new HashSet<>());
+			races.put(race.getId(), teams = new ArrayList<>());
 		Team team = new Team();
 		team.setClub(club);
 		List<TeamMember> members = new ArrayList<>();
