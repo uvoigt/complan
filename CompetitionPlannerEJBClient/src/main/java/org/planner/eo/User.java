33b9bcd60546ecefa8812c2cdcaca400fb41576c
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-17 22:41:35 +0200

    age helper

--- CompetitionPlannerEJBClient/src/main/java/org/planner/eo/User.java
+++ CompetitionPlannerEJBClient/src/main/java/org/planner/eo/User.java
@@ -1,5 +1,6 @@
 package org.planner.eo;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -17,6 +18,9 @@ import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.joda.time.LocalDate;
+import org.joda.time.Years;
+import org.planner.model.AgeType;
 import org.planner.model.Gender;
 import org.planner.util.NLSBundle;
 import org.planner.util.Visible;
@@ -185,4 +189,53 @@ public class User extends AbstractEntity {
 	public void setGender(Gender gender) {
 		this.gender = gender;
 	}
+
+	public String getName() {
+		return firstName + " " + lastName;
+	}
+
+	public int getAge() {
+		if (birthDate == null)
+			return 0;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(birthDate);
+		return Years.yearsBetween(new LocalDate(birthDate), new LocalDate()).getYears();
+	}
+
+	public AgeType getAgeType() {
+		if (birthDate == null)
+			return null;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(birthDate);
+		// hier ist ggf. nicht das exakte Alter gefragt, sondern, ob jemand in diesem Jahr das Alter erreicht
+		int age = Calendar.getInstance().get(Calendar.YEAR) - cal.get(Calendar.YEAR);
+		switch (age) {
+		case 11:
+			return AgeType.schuelerC;
+		case 12:
+			return AgeType.schuelerB;
+		case 13:
+			return AgeType.schuelerA;
+		case 14:
+		case 15:
+			return AgeType.jugend;
+		case 16:
+		case 17:
+			return AgeType.junioren;
+		}
+		if (age < 32)
+			return AgeType.lk;
+		if (age < 40)
+			return AgeType.akA;
+		if (age < 50)
+			return AgeType.akB;
+		if (age < 60)
+			return AgeType.akC;
+		if (age < 70)
+			return AgeType.akD;
+		if (age < 80)
+			return AgeType.akE;
+		throw new IllegalArgumentException("sorry, this age is not implemented");
+	}
+
 }
