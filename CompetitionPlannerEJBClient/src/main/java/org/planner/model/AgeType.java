c2883c90722f2dbc462c525a97a982c10c59b831
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-17 22:39:27 +0200

    helper-Methoden

--- CompetitionPlannerEJBClient/src/main/java/org/planner/model/AgeType.java
+++ CompetitionPlannerEJBClient/src/main/java/org/planner/model/AgeType.java
@@ -3,10 +3,31 @@ package org.planner.model;
 import org.planner.util.CommonMessages;
 
 public enum AgeType implements LocalizedEnum {
-	schuelerA, schuelerB, schuelerC, jugend, junioren, lk, akA, akB, akC, akD, ak;
+	schuelerA, schuelerB, schuelerC, jugend, junioren, lk, akA, akB, akC, akD, akE, ak;
 
 	@Override
 	public String getText() {
 		return CommonMessages.getEnumText(this);
+	}
+
+	public boolean isMature() {
+		switch (this) {
+		default:
+			throw new IllegalArgumentException(name());
+		case schuelerA:
+		case schuelerB:
+		case schuelerC:
+		case jugend:
+			return false;
+		case junioren:
+		case lk:
+		case akA:
+		case akB:
+		case akC:
+		case akD:
+		case akE:
+		case ak:
+			return true;
+		}
 	}
 }
--- CompetitionPlannerEJBClient/src/main/java/org/planner/model/Gender.java
+++ CompetitionPlannerEJBClient/src/main/java/org/planner/model/Gender.java
@@ -3,10 +3,14 @@ package org.planner.model;
 import org.planner.util.CommonMessages;
 
 public enum Gender implements LocalizedEnum {
-	m, w, mixed;
+	m, f, mixed;
 
 	@Override
 	public String getText() {
 		return CommonMessages.getEnumText(this);
+	}
+
+	public String getAgeFriendlyText(AgeType ageType) {
+		return CommonMessages.getMessage(getClass().getSimpleName() + "." + name() + (ageType.isMature() ? "m" : "c"));
 	}
 }
