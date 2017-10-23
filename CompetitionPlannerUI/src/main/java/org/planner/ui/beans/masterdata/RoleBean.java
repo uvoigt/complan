d21b17c33e8a09bb6ae2215c8f17985bd445c3bf
Uwe <17613742+uvoigt@users.noreply.github.com>
2017-10-23 18:25:55 +0200

    role helper au dem Rolebean rausgenommen, da es sonst unnötigerweise initialisiert wurde

--- CompetitionPlannerUI/src/main/java/org/planner/ui/beans/masterdata/RoleBean.java
+++ CompetitionPlannerUI/src/main/java/org/planner/ui/beans/masterdata/RoleBean.java
@@ -2,12 +2,11 @@ package org.planner.ui.beans.masterdata;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import org.planner.eo.Role;
 import org.planner.ui.beans.AbstractEditBean;
+import org.planner.ui.util.JsfUtil;
 
 @Named
 @RequestScoped
@@ -20,10 +19,14 @@ public class RoleBean extends AbstractEditBean {
 	@PostConstruct
 	public void init() {
 		Long id = getIdFromRequestParameters();
-		if (id != null)
+		if (id == null)
+			id = (Long) JsfUtil.getViewVariable("id");
+		if (id != null) {
 			role = service.getObject(Role.class, id, 0);
-		else
+			JsfUtil.setViewVariable("id", role.getId());
+		} else {
 			role = new Role();
+		}
 	}
 
 	@Override
@@ -38,30 +41,5 @@ public class RoleBean extends AbstractEditBean {
 	@Override
 	protected void doSave() {
 		service.saveRole(role);
-	}
-
-	public boolean inRole(String role) {
-		ExternalContext ctx = FacesContext.getCurrentInstance().getExternalContext();
-		return ctx.isUserInRole(role);
-	}
-
-	public boolean inRole(String role1, String role2) {
-		ExternalContext ctx = FacesContext.getCurrentInstance().getExternalContext();
-		return ctx.isUserInRole(role1) || ctx.isUserInRole(role2);
-	}
-
-	public boolean inRole(String role1, String role2, String role3) {
-		ExternalContext ctx = FacesContext.getCurrentInstance().getExternalContext();
-		return ctx.isUserInRole(role1) || ctx.isUserInRole(role2) || ctx.isUserInRole(role3);
-	}
-
-	public boolean notInRole(String role) {
-		ExternalContext ctx = FacesContext.getCurrentInstance().getExternalContext();
-		return !ctx.isUserInRole(role);
-	}
-
-	public boolean notInRole(String role1, String role2) {
-		ExternalContext ctx = FacesContext.getCurrentInstance().getExternalContext();
-		return !ctx.isUserInRole(role1) && !ctx.isUserInRole(role2);
 	}
 }
