8ec7fdf642d36726fd1efe9f058756cf48e63358
Uwe <17613742+uvoigt@users.noreply.github.com>
2019-02-10 00:54:17 +0100

    Test-DB in-Memory

--- CompetitionPlannerEJBClient/src/test/java/org/planner/util/EmTestUtil.java
+++ CompetitionPlannerEJBClient/src/test/java/org/planner/util/EmTestUtil.java
@@ -1,10 +1,5 @@
 package org.planner.util;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -14,12 +9,8 @@ public class EmTestUtil {
 	private static EntityManagerFactory emf;
 
 	public static void startup() {
-		Map<String, String> properties = new HashMap<>();
-		properties.put("hibernate.connection.username", "sa");
-		properties.put("hibernate.connection.password", "sa");
-		properties.put("hibernate.connection.url", "jdbc:h2:./unittestdb");
 		try {
-			emf = Persistence.createEntityManagerFactory("PLANNER_PU", properties);
+			emf = Persistence.createEntityManagerFactory("PLANNER_PU");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -28,20 +19,6 @@ public class EmTestUtil {
 	public static void shutdown() {
 		if (emf != null)
 			emf.close();
-		deleteDB();
-	}
-
-	private static void deleteDB() {
-		File dir = new File(".");
-		FilenameFilter filter = new FilenameFilter() {
-			@Override
-			public boolean accept(File dir, String name) {
-				return name.startsWith("unittestdb");
-			}
-		};
-		for (File file : dir.listFiles(filter)) {
-			file.delete();
-		}
 	}
 
 	public static EntityManager createEntityManager() {
